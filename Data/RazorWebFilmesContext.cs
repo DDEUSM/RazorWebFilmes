424ff8ddf419f2ebf48dda31e1252213a6326fd4
PCHome <daviddeusmesquita@gmail.com>
2023-10-26 20:32:29 -0300

    Adicionar arquivos de projeto.

---
+++ Data/RazorWebFilmesContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorWebFilmes.Models;
+
+namespace RazorWebFilmes.Data
+{
+    public class RazorWebFilmesContext : DbContext
+    {
+        public RazorWebFilmesContext (DbContextOptions<RazorWebFilmesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorWebFilmes.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorWebFilmes.Data;
+using RazorWebFilmes.Models;
+
+namespace RazorWebFilmes.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RazorWebFilmes.Data.RazorWebFilmesContext _context;
+
+        public IndexModel(RazorWebFilmes.Data.RazorWebFilmesContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Movie != null)
+            {
+                Movie = await _context.Movie.ToListAsync();
+            }
+        }
+    }
+}
